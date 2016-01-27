1f7888c3e9c012c74ba250996c92969d808ef309
Peter Yoachim <yoachim@uw.edu>
2016-01-27 11:46:08 -0800

    try this

--- tests/testHourglassMetric.py
+++ tests/testHourglassMetric.py
@@ -1,34 +1,31 @@
-import matplotlib
-matplotlib.use("Agg")
 import numpy as np
-import healpy as hp
 import unittest
 import lsst.sims.maf.metrics as metrics
-import lsst.sims.maf.stackers as stackers
 
-'''
+
 class TestHourglassmetric(unittest.TestCase):
-    # Bizare random fails again
 
     def testHourglassMetric(self):
         """Test the hourglass metric """
-        names = [ 'expMJD', 'night','filter']
-        types = [float,float,str]
-        data = np.zeros(10, dtype = zip(names,types))
-        data['night'] = np.round(np.arange(0, 2, .1))[:10]
-        data['expMJD'] = np.sort(np.random.rand(10)) + data['night']
-        data['filter'] = 'r'
-        slicePoint = [0]
-        metric = metrics.HourglassMetric()
-        result = metric.run(data, slicePoint)
-        pernight = result['pernight']
-        perfilter = result['perfilter']
-        # Check that the format is right at least
-        assert(np.size(perfilter) == 2*data.size)
-        assert(len(pernight.dtype.names) == 9)
+        names = ['expMJD', 'night', 'filter']
+        types = [float, float, '|S1']
+        for npts in [10,100,1000]:
+            data = np.zeros(npts, dtype=zip(names, types))
+            data['night'] = np.round(np.arange(0, 5, 1./npts))[:npts]+1
+            data['expMJD'] = np.arange(0, 5, 1./npts)[:npts] + 59580 + data['night']
+
+            data['filter'] = 'r'
+            slicePoint = [0]
+            metric = metrics.HourglassMetric()
+            result = metric.run(data, slicePoint)
+            pernight = result['pernight']
+            perfilter = result['perfilter']
+            # Check that the format is right at least
+            assert(np.size(perfilter) > 2)
+            assert(np.size(pernight) == np.size(np.unique(data['night'])))
+            assert(len(pernight.dtype.names) == 9)
 
 
 if __name__ == '__main__':
 
     unittest.main()
-'''
